daf69d12edb7df3bf338bbd10e6b86464ef0272e
GabrielJonesCA <gabrieljones900@yahoo.com>
2025-01-20 11:31:40 -0800

    made movie card jsx file
    created a basic jsx file to export for the movie card object

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,9 @@ function App() {
 
   return (
     <>
-      <Text display="hello"/>
-      <Text display= "whats up"/>
+      
     </>
   )
-}
-function Text({display}) {
-  return <div>
-    <p>{display}</p>
-  </div>
+
 }
 export default App
